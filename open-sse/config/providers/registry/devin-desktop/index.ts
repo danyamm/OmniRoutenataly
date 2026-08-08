DorianGallo/OmniRoute

feat(devin-desktop): replace public Windsurf provider

open-sse/config/providers/registry/devin-desktop/index.ts
@@ -1,0 +1,14 @@
+import type { RegistryEntry } from "../../shared.ts";
+import { DEVIN_MODEL_CATALOG } from "../devin/catalog.ts";
+
+export const devin_desktopProvider: RegistryEntry = {
+  id: "devin-desktop",
+  format: "windsurf",
+  executor: "devin-desktop",
+  baseUrl: "https://server.codeium.com",
+  authType: "oauth",
+  authHeader: "Authorization",
+  authPrefix: "Bearer ",
+  defaultContextLength: 200000,
+  models: DEVIN_MODEL_CATALOG,
+};

src/shared/components/lobeProviderIcons.ts
@@ -468,7 +468,7 @@ const LOBE_PROVIDER_ALIASES = {
   voyage: "Voyage",
   "voyage-ai": "Voyage",
   watsonx: "IBM",
-  windsurf: "Windsurf",
+  "devin-desktop": "Windsurf",
   "workers-ai": "WorkersAI",
   workersai: "WorkersAI",
   xai: "XAI",

danyamm/OmniRoutenataly

open-sse/config/providers/registry/devin-desktop/index.ts
@@ -1,0 +1,14 @@
+import type { RegistryEntry } from "../../shared.ts";
+import { DEVIN_MODEL_CATALOG } from "../devin/catalog.ts";
+
+export const devin_desktopProvider: RegistryEntry = {
+  id: "devin-desktop",
+  format: "openai",
+  executor: "devin-desktop",
+  baseUrl: "https://server.codeium.com",
+  authType: "oauth",
+  authHeader: "Authorization",
+  authPrefix: "Bearer ",
+  defaultContextLength: 200000,
+  models: DEVIN_MODEL_CATALOG,
+};

src/shared/components/lobeProviderIcons.ts
@@ -468,7 +468,7 @@ const LOBE_PROVIDER_ALIASES = {
   voyage: "Voyage",
   "voyage-ai": "Voyage",
   watsonx: "IBM",
-  windsurf: "Windsurf",
+  "devin-desktop": "Devin",
   "workers-ai": "WorkersAI",
   workersai: "WorkersAI",
   xai: "XAI",